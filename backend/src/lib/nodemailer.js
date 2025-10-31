f10bad2cb5954273797d7aed8e4d901cefca2809
userDoffy <elitedoffy@gmail.com>
2025-10-31 12:30:33 +0545

    nodemailer update

--- backend/src/lib/nodemailer.js
+++ backend/src/lib/nodemailer.js
@@ -8,7 +8,8 @@ const transporter = nodemailer.createTransport({
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASSWORD,
-    }
+    },
+    secure: false,
 });
 
 export const sendOtpEmail = async (to, otp) => {
